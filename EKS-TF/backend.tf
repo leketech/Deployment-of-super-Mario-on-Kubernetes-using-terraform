95820a7e1efb8b9d0f4d6b4e62587b285beefadf
Aduraleke Faith Akintade <103920213+leketech@users.noreply.github.com>
2024-05-25 07:47:47 +0100

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "mario12bucket" # Replace with your actual S3 bucket name
+    bucket = "smario" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "eu-central-1"
+    region = "us-east-1"
   }
 }
